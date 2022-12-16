0d79d321482ee382cb68faaa25d807fa8587c44a
muco75 <118021397+muco75@users.noreply.github.com>
2022-12-17 00:43:50 +0100

    my first VUE project,
    This is a ToDoApp project.

---
+++ vuejs/vueproject1/vueproject1/VueBackend/Controllers/ToDoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VueBackend.data;
+using VueBackend.models;
+
+namespace VueBackend.Controllers
+
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : Controller
+    {
+        private readonly ApplicationDbContext _database;
+        public ToDoController(ApplicationDbContext db)
+        {
+            _database = db;
+        }
+
+
+        [HttpGet]
+        [Route("get")]
+        public List<item> GetTask()
+        {
+            var items = _database.Items.ToList();
+            return items;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public ActionResult AddTask([FromBody] item item)
+        {
+            _database.Items.Add(item);
+            _database.SaveChanges();
+            return new OkResult();
+        }
+
+        // Update(changes)
+
+        [HttpPost]
+        [Route("update/{id}")]
+        public ActionResult changeTask(int? id, [FromBody] item item)
+        {
+
+            //item ophalen van dataabse
+            var entity = _database.Items.FirstOrDefault(item => item.Id == id);
+            if (entity != null)
+                {
+                // item veranderen 
+
+                entity.Status = item.Status;
+                entity.Task = item.Task;
+
+
+            }
+            
+            _database.SaveChanges();
+            return new OkResult();
+        }
+
+
+        //post delete
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        //[ValidateAntiForgeryToken]
+        public IActionResult PostDelete(int? id)
+
+        {
+            var obj = _database.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _database.Items.Remove(obj);
+            _database.SaveChanges();
+            return new OkResult();
+        }
+
+    }
+
+}
---
+++ vuejs/vueproject1/vueproject1/VueBackend/Migrations/20220721111349_addTasksToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VueBackend.Migrations
+{
+    public partial class addTasksToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Task = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Task);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
