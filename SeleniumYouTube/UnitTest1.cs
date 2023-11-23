5c81502293fdbc6441135d06ab6993f86de41013
muco75 <118021397+muco75@users.noreply.github.com>
2023-11-23 17:40:03 +0100

    A selenium youtube test project
    It search a video, skip add, plays video

---
+++ SeleniumYouTube/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumYouTube
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://www.youtube.com");
+
+            Driver.FindElement(By.XPath("/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")).Click();
+            Driver.FindElement(By.XPath("//*[@id=\"search-form\"]")).Click();
+            Driver.FindElement(By.Name("search_query")).SendKeys("mahir bakara");
+            Driver.FindElement(By.Id("search-icon-legacy")).Click();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            Driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/div/div[1]/div/h3/a")).Click();
+
+
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            Driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[21]/div/div[3]/div/div[2]/span/button")).Click();
+
+
+
+
+            Assert.Pass();
+        }
+    }
+}
